22615eacc71de447ae8f286cf5810c5ad5266b18
RipaIslam <tanginaislam520@gmail.com>
2021-09-18 12:31:22 +0600

    DDF, DD_LoginTest, DataDriven, xls_Reader

--- src/main/java/com/BrowserOptions/BrowserBinary.java
+++ src/main/java/com/BrowserOptions/BrowserBinary.java
@@ -16,7 +16,7 @@ public class BrowserBinary extends TestBase {
     public static void setBinary(){
         System.setProperty("webdriver.gecko.driver","./src/main/resources/geckodriver.exe");
         FirefoxOptions frxObj = new FirefoxOptions();
-        frxObj.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox")));
+        frxObj.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
         driver = new FirefoxDriver();
         driver.get("https://google.com/");
     }
